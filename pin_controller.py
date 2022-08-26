5cb5d73d87501f5c007eba9472c7836e7feed17a
gdevincenti <72762133+gdevincenti@users.noreply.github.com>
2022-08-25 21:16:49 -0300

    Add files via upload

--- pin_controller.py
+++ pin_controller.py
@@ -2,12 +2,12 @@ import RPi.GPIO as GPIO
 #import random
 #import time
 
-pins = [{'pin_num': 17, 'canal': '1'},
-        {'pin_num': 18, 'canal': '2'},
-        {'pin_num': 22, 'canal': '3'},
-        {'pin_num': 23, 'canal': '4'}]
+pins = [{'pin_num': 17, 'canal': 1},
+        {'pin_num': 18, 'canal': 2},
+        {'pin_num': 22, 'canal': 3},
+        {'pin_num': 23, 'canal': 4}]
 
-#GPIO.setmode(GPIO.BCM)  # use GPIO numbering, not generic
+GPIO.setmode(GPIO.BCM)  # use GPIO numbering, not generic
 #GPIO.setwarnings(False)
 
 # setup all pins based on above configuration
@@ -15,7 +15,7 @@ for pin in pins:
     GPIO.setup(pin['pin_num'], GPIO.OUT, initial=GPIO.LOW)
 
 
-def toggle_canal(canal: str, state: str):
+def toggle_canal(canal: int, state: str):
     for pin in pins:
         if pin['canal'] == canal:
             if state == 'on':
@@ -24,11 +24,11 @@ def toggle_canal(canal: str, state: str):
                 GPIO.output(pin['pin_num'], GPIO.LOW)
 
 
-def canal_on(canal: str):
+def canal_on(canal: int:
     toggle_canal(canal, 'on')
 
 
-def canal_off(canal: str):
+def canal_off(canal: int):
     toggle_canal(canal, 'off')
 
 
