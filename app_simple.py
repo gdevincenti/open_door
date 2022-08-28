6e8027398f6d5fb3366642553c36ee1f63dd3112
gdevincenti <gdevincenti97@gmail.com>
2022-08-27 22:09:37 -0300

    Create app_simple.py

---
+++ app_simple.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restx import Api, Resource, fields
+import pin_controller as pc
+app = Flask(__name__)
+api = Api(app)
+class Helloworld(Resource):
+	def __init__(self):
+		pass
+	def get(self):
+		return {
+			"Hello": "World"
+		}
+api.add_resource(Helloworld, '/')
+if __name__ == '__main__':
+	app.run(host="0.0.0.0", debug=True)
